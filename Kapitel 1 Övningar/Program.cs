82e7ec612be3a821177c2ae18862696cb6c99e7b
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-06 10:16:04 +0200

    Add project files.

---
+++ Kapitel 1 Övningar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Inlämningsuppgift_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.\nDu har lärt dig skriva ut tecken som \" och \\.");
+        }
+    }
+}
+
+
