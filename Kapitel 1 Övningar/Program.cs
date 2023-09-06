be09d7fe1dd92ea289fdbe4ff10628f36c31e57f
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-06 10:24:27 +0200

    Lade till kommentarer

--- Kapitel 1 Övningar/Program.cs
+++ Kapitel 1 Övningar/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-namespace Inlämningsuppgift_1_1
+﻿using System; //Importerar ett paket
+namespace Inlämningsuppgift_1_1 //Ett arbiträrt namn till programmet. 
 {
-    class Program
+    class Program // Detta är en datastruktur som kombinerar variablar och funktioner. 
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Detta är den första metoden som körs i samband med att programmet startar. 
         {
-            Console.WriteLine("Du är nästan klar med kapitel 1.\nDu har lärt dig skriva ut tecken som \" och \\.");
+            Console.WriteLine("Du är nästan klar med kapitel 1.\nDu har lärt dig skriva ut tecken som \" och \\."); //Console.WriteLine printar ut texten i konsolen.  
         }
     }
 }
